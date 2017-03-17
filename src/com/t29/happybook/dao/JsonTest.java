2580ab57593282f413d27a49eaca69b6af3f0d76
jack <zhengyongqiang@yeah.net>
2017-03-17 21:59:14 +0800

    Revert "Revert "测试JSON对象""
    This reverts commit 24602c2d3cb12431f6e22250473c2aa7ac238f46.

--- src/com/t29/happybook/dao/JsonTest.java
+++ src/com/t29/happybook/dao/JsonTest.java
@@ -17,13 +17,17 @@ public class JsonTest {
 	public static void main(String[] args) throws JSONException {
 		List<Book> books = new ArrayList<Book>();
 		Book book = new Book(1, "三国演义", "罗贯中", 56.8, "人民教育出版社", "default.jpg", new Category(1001, "小说"));
-		Book book1 = new Book(2, "三义", "罗中", 5.8, "育出版社", "default.jpg", new Category(1002, "小说"));
-		Book book2 = new Book(3, "国演", "贯中", 6.8, "人民教育社", "default.jpg", new Category(1003, "小说"));
+		Book book1 = new Book(2, "西游记", "吴承恩", 45.8, "育出版社", "default.jpg", new Category(1002, "小说"));
+		Book book2 = new Book(3, "红楼梦", "曹雪芹", 26.8, "人民教育社", "default.jpg", new Category(1003, "小说"));
+		Book book3 = new Book(4, "水浒传", "施耐庵", 36.8, "机械工业出版社", "default.jpg", new Category(1004, "小说"));
 		books.add(book);
 		books.add(book1);
 		books.add(book2);
+		books.add(book3);
+		
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("book", book);
+		jsonObject.put("books", books);
 		
 		System.out.println(jsonObject);
 		
